6b0659230db1aa81724a1672ae20cbf21ad00598
Bin Chen <binchen@Bins-MacBook-Pro-2.local>
2018-09-22 13:32:44 -0400

    committing components

--- ywait-app/src/app/app.module.ts
+++ ywait-app/src/app/app.module.ts
@@ -3,11 +3,29 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MerchantComponent } from './merchant/merchant.component';
+import { CouponListComponent } from './coupon-list/coupon-list.component';
+import { AboutComponent } from './about/about.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+import { HeaderComponent } from './navigation/header/header.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MerchantComponent,
+    LoginComponent,
+    CouponListComponent,
+    AboutComponent,
+    UserProfileComponent,
+    FooterComponent,
+    SidenavListComponent,
+    HeaderComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule
